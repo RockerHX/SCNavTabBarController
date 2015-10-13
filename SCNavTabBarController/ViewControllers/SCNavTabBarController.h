226b1f3f28411f643ebdb1bcadb63a49d4809f18
GevinChen <lowgoo@gmail.com>
2015-10-13 14:59:03 +0800

    1. 加入 autolayout 2. 改為可指定內容顯於特定的 view，而非 full screen 3. 多一些屬性，可以控制外觀，例如 tab line height, shadow hide, tab bar text color , tab bar bg color

--- SCNavTabBarController/ViewControllers/SCNavTabBarController.h
+++ SCNavTabBarController/ViewControllers/SCNavTabBarController.h
@@ -15,12 +15,17 @@
 @property (nonatomic, assign)   BOOL        canPopAllItemMenu;          // Default value: YES
 @property (nonatomic, assign)   BOOL        scrollAnimation;            // Default value: NO
 @property (nonatomic, assign)   BOOL        mainViewBounces;            // Default value: NO
+@property (nonatomic, assign)   BOOL        dragToSwitchView;            // Default value: YES // 2015-03-09 Gevin Added
 
 @property (nonatomic, strong)   NSArray     *subViewControllers;        // An array of children view controllers
 
 @property (nonatomic, strong)   UIColor     *navTabBarColor;            // Could not set [UIColor clear], if you set, NavTabbar will show initialize color
 @property (nonatomic, strong)   UIColor     *navTabBarLineColor;
 @property (nonatomic, strong)   UIImage     *navTabBarArrowImage;
+@property (nonatomic, assign)   UIView      *containerView;
+@property (nonatomic, strong)   UIColor     *navTabBarTextColor;        // Gevin added
+@property (nonatomic)           BOOL        showShadow;                 // Gevin added
+@property (nonatomic)           float       navTabBarLineHeight;        // Gevin added
 
 /**
  *  Initialize Methods
@@ -48,6 +53,7 @@
  *  @return Instance
  */
 - (id)initWithParentViewController:(UIViewController *)viewController;
+- (id)initWithParentViewController:(UIViewController *)viewController containerView:(UIView*)containerView;
 
 /**
  *  Initialize SCNavTabBarViewController Instance, Show On The Parent View Controller And Show On The Parent View Controller
@@ -55,10 +61,12 @@
  *  @param subControllers - set an array of children view controllers
  *  @param viewController - set parent view controller
  *  @param can            - can pop all item menu
+ *  @param containerView  - set view to display content of child view controllers // Add by Gevin
  *
  *  @return Instance
  */
 - (id)initWithSubViewControllers:(NSArray *)subControllers andParentViewController:(UIViewController *)viewController canPopAllItemMenu:(BOOL)can;
+- (id)initWithSubViewControllers:(NSArray *)subControllers andParentViewController:(UIViewController *)viewController containerView:(UIView*)containerView canPopAllItemMenu:(BOOL)can;
 
 /**
  *  Show On The Parent View Controller
@@ -66,5 +74,15 @@
  *  @param viewController - set parent view controller
  */
 - (void)addParentController:(UIViewController *)viewController;
+
+/**
+ *  Show specify index of child view controller
+ *
+ *  @param index - set container view display index of child view controller
+ */
+- (void)itemDidSelectedWithIndex:(NSInteger)index;
+
+// Gevin Added
+- (NSInteger)getCurrentIndex;
 
 @end
--- SCNavTabBarController/ViewControllers/SCNavTabBarController.m
+++ SCNavTabBarController/ViewControllers/SCNavTabBarController.m
@@ -17,6 +17,8 @@
     
     SCNavTabBar     *_navTabBar;                // NavTabBar: press item on it to exchange view
     UIScrollView    *_mainView;                 // content view
+    
+    
 }
 
 @end
@@ -46,21 +48,33 @@
     return self;
 }
 
-- (id)initWithParentViewController:(UIViewController *)viewController
+- (id)initWithParentViewController:(UIViewController *)viewController {
+    return [self initWithParentViewController:viewController containerView:viewController.view];
+}
+
+- (id)initWithParentViewController:(UIViewController *)viewController containerView:(UIView *)containerView
 {
     self = [super init];
     if (self)
     {
-        [self addParentController:viewController];
+        _showArrowButton = NO;
+        _containerView = containerView;
+        [self addParentController:viewController ];
     }
     return self;
 }
 
-- (id)initWithSubViewControllers:(NSArray *)subControllers andParentViewController:(UIViewController *)viewController canPopAllItemMenu:(BOOL)can;
+- (id)initWithSubViewControllers:(NSArray *)subControllers andParentViewController:(UIViewController *)viewController canPopAllItemMenu:(BOOL)can
+{
+    return [self initWithSubViewControllers:subControllers andParentViewController:viewController containerView:viewController.view showArrowButton:show ];
+}
+
+- (id)initWithSubViewControllers:(NSArray *)subControllers andParentViewController:(UIViewController *)viewController containerView:(UIView *)containerView canPopAllItemMenu:(BOOL)can
 {
     self = [self initWithSubViewControllers:subControllers];
     
     _canPopAllItemMenu = can;
+    _containerView = containerView;
     [self addParentController:viewController];
     return self;
 }
@@ -69,29 +83,66 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
     [self initConfig];
     [self viewConfig];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    // Gevin added : 2015-10-06
+    //  case:
+    //  假設 SCNavTabbarController 有三個 tab，目前顯示第二個 tab
+    //  接著，進入下一個 controller，之後再回到 SCNavTabbarController
+    //  這時，SCNavTabbarController 的 tab 仍然會停在第二個 tab，但是顯示內容卻是第一個 tab 的內容
+    //  
+    //  原因：
+    //  _mainView(UIScrollView) 的 offset 在 viewDidLayoutSubviews 時被重置為 0 了
+    //  但是上方 tab button 的 focus 還是在第二頁，找不到原因，所以多這個修正
+    [_mainView setContentOffset:(CGPoint){ _currentIndex * SCREEN_WIDTH, 0 }];
+//    NSLog(@"1 main view offset %@", NSStringFromCGPoint( _mainView.contentOffset ) );
 }
+
+//- (void)viewWillLayoutSubviews{
+//    [super viewWillLayoutSubviews];
+//    NSLog(@"1a main view offset %@", NSStringFromCGPoint( _mainView.contentOffset ) );
+//}
+
+- (void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    // Gevin added 2015-07-28 處理 _mainView.contentOffset 的問題，可能是因為 autolayout 的關係，在 viewDidLayoutSubviews 之後，先前的設定都會被重置
+    [self itemDidSelectedWithIndex: _currentIndex ];
+//    NSLog(@"1b main view offset %@", NSStringFromCGPoint( _mainView.contentOffset ) );
+}
+
+//- (void)viewDidAppear:(BOOL)animated{
+//    [super viewDidAppear:animated];
+//    NSLog(@"2 main view offset %@", NSStringFromCGPoint( _mainView.contentOffset ) );
+//}
+
+//- (void)viewWillDisappear:(BOOL)animated
+//{
+//    [super viewWillDisappear:animated];
+//    NSLog(@"3 main view offset %@", NSStringFromCGPoint( _mainView.contentOffset ) );
+//}
 
 #pragma mark - Private Methods
 #pragma mark -
 - (void)initConfig
 {
     // Iinitialize value
-    _currentIndex = 1;
+    _currentIndex = 0;
     _navTabBarColor = _navTabBarColor ? _navTabBarColor : NavTabbarColor;
+    
+    // Gevin added
+    _dragToSwitchView = YES;
+    _showShadow = YES;
     
     // Load all title of children view controllers
     _titles = [[NSMutableArray alloc] initWithCapacity:_subViewControllers.count];
     for (UIViewController *viewController in _subViewControllers)
     {
+        if( viewController.title == nil ) NSLog(@"error!! controller title is nil.");
         [_titles addObject:viewController.title];
     }
 }
@@ -99,22 +150,36 @@
 - (void)viewInit
 {
     // Load NavTabBar and content view to show on window
-    _navTabBar = [[SCNavTabBar alloc] initWithFrame:CGRectMake(DOT_COORDINATE, DOT_COORDINATE, SCREEN_WIDTH, NAV_TAB_BAR_HEIGHT) canPopAllItemMenu:_canPopAllItemMenu];
+    _navTabBar = [[SCNavTabBar alloc] initWithFrame:CGRectMake(DOT_COORDINATE, DOT_COORDINATE, SCREEN_WIDTH, NAV_TAB_BAR_HEIGHT) showArrowButton:_showArrowButton];
     _navTabBar.delegate = self;
-    _navTabBar.backgroundColor = _navTabBarColor;
+    _navTabBar.naviColor = _navTabBarColor;
     _navTabBar.lineColor = _navTabBarLineColor;
     _navTabBar.itemTitles = _titles;
     _navTabBar.arrowImage = _navTabBarArrowImage;
+    _navTabBar.showShadow = _showShadow; // Gevin added
     [_navTabBar updateData];
-    
+
     _mainView = [[UIScrollView alloc] initWithFrame:CGRectMake(DOT_COORDINATE, _navTabBar.frame.origin.y + _navTabBar.frame.size.height, SCREEN_WIDTH, SCREEN_HEIGHT - _navTabBar.frame.origin.y - _navTabBar.frame.size.height - STATUS_BAR_HEIGHT - NAVIGATION_BAR_HEIGHT)];
     _mainView.delegate = self;
     _mainView.pagingEnabled = YES;
     _mainView.bounces = _mainViewBounces;
     _mainView.showsHorizontalScrollIndicator = NO;
-    _mainView.contentSize = CGSizeMake(SCREEN_WIDTH * _subViewControllers.count, DOT_COORDINATE);
+    // 2015-03-09 Gevin Added
+    int cnt = 1;
+    if ( _dragToSwitchView ) {
+        cnt = (int)_subViewControllers.count;
+    }
+    _mainView.contentSize = CGSizeMake(SCREEN_WIDTH * cnt , DOT_COORDINATE);
     [self.view addSubview:_mainView];
     [self.view addSubview:_navTabBar];
+    
+    _navTabBar.translatesAutoresizingMaskIntoConstraints = NO;
+    _mainView.translatesAutoresizingMaskIntoConstraints = NO;
+    //Gevin added for autolayout
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|[_navTabBar]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_navTabBar)]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|[_mainView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_mainView)]];
+    NSString* statement = [NSString stringWithFormat:@"V:|[_navTabBar(%f)]-[_mainView]|",NAVIGATION_BAR_HEIGHT];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:statement options:0 metrics:nil views:NSDictionaryOfVariableBindings(_navTabBar,_mainView)]];
 }
 
 - (void)viewConfig
@@ -128,47 +193,106 @@
         viewController.view.frame = CGRectMake(idx * SCREEN_WIDTH, DOT_COORDINATE, SCREEN_WIDTH, _mainView.frame.size.height);
         [_mainView addSubview:viewController.view];
         [self addChildViewController:viewController];
+        
+        // gevin added trigger autolayout of viewController.view
+        [viewController.view layoutIfNeeded];
+        viewController.view.frame = CGRectMake(idx * SCREEN_WIDTH, DOT_COORDINATE, SCREEN_WIDTH, _mainView.frame.size.height);
+        
     }];
 }
 
 #pragma mark - Public Methods
 #pragma mark -
-- (void)setNavTabbarColor:(UIColor *)navTabbarColor
+
+- (void)setNavTabBarLineHeight:(float)navTabBarLineHeight{
+    _navTabBarLineHeight = navTabBarLineHeight;
+    _navTabBar.lineHeight = _navTabBarLineHeight;
+}
+
+- (void)setShowShadow:(BOOL)showShadow{
+    _showShadow = showShadow;
+    if( _navTabBar ) _navTabBar.showShadow = showShadow;
+}
+
+- (void)setNavTabBarColor:(UIColor *)navTabBarColor
 {
     // prevent set [UIColor clear], because this set can take error display
     CGFloat red, green, blue, alpha;
-    if ([navTabbarColor getRed:&red green:&green blue:&blue alpha:&alpha] && !red && !green && !blue && !alpha)
+    if ([navTabBarColor getRed:&red green:&green blue:&blue alpha:&alpha] && !red && !green && !blue && !alpha)
     {
-        navTabbarColor = NavTabbarColor;
+        navTabBarColor = NavTabbarColor;
     }
-    _navTabBarColor = navTabbarColor;
+    _navTabBarColor = navTabBarColor;
+    _navTabBar.naviColor = _navTabBarColor;
 }
 
+- (void)setNavTabBarLineColor:(UIColor *)navTabBarLineColor{
+    _navTabBarLineColor = navTabBarLineColor;
+    _navTabBar.lineColor = navTabBarLineColor;
+}
+
+- (void)setNavTabBarTextColor:(UIColor *)navTabBarTextColor{
+    _navTabBarTextColor = navTabBarTextColor;
+    _navTabBar.textColor = _navTabBarTextColor;
+}
+
+
+
+// modify by Gevin ，多一個 container view，因為有可能不是要滿版的，containerView 必須要是 viewController 的
 - (void)addParentController:(UIViewController *)viewController
 {
+    // Gevin modify: 如果加這行，會把 SCNavTabBarController 的 parent Controller 的 view 下移
     // Close UIScrollView characteristic on IOS7 and later
-    if ([viewController respondsToSelector:@selector(edgesForExtendedLayout)])
+    if ( _containerView == viewController.view && [viewController respondsToSelector:@selector(edgesForExtendedLayout)])
     {
         viewController.edgesForExtendedLayout = UIRectEdgeNone;
     }
     
     [viewController addChildViewController:self];
-    [viewController.view addSubview:self.view];
+    [_containerView addSubview:self.view];
+    
+    // Gevin added for autolayout
+    self.view.translatesAutoresizingMaskIntoConstraints = NO;
+    UIView* view = self.view;
+    [_containerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[view]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(view)]];
+    [_containerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[view]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(view)]];
+
+}
+
+// 2015-03-09 Gevin Added
+- (void)setDragToSwitchView:(BOOL)dragToSwitchView
+{
+    _dragToSwitchView = dragToSwitchView;
+    
+    int cnt = 1;
+    if ( _dragToSwitchView ) {
+        cnt = (int)_subViewControllers.count;
+    }
+    _mainView.contentSize = CGSizeMake(SCREEN_WIDTH * cnt , DOT_COORDINATE);
+    
 }
 
 #pragma mark - Scroll View Delegate Methods
 #pragma mark -
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView
-{
-    _currentIndex = scrollView.contentOffset.x / SCREEN_WIDTH;
-    _navTabBar.currentItemIndex = _currentIndex;
-}
+
+//- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+//{
+//    _currentIndex = scrollView.contentOffset.x / SCREEN_WIDTH;
+//    NSLog(@"SCNavTabBarController .... scrollView did scroll");
+//}
 
 #pragma mark - SCNavTabBarDelegate Methods
 #pragma mark -
 - (void)itemDidSelectedWithIndex:(NSInteger)index
 {
+    // 2015-03-13 Gevin Added，切換 sub controller 要呼叫相映method
+//    UIViewController *vc = _subViewControllers[index];
+//    [vc viewWillAppear:YES];
     [_mainView setContentOffset:CGPointMake(index * SCREEN_WIDTH, DOT_COORDINATE) animated:_scrollAnimation];
+//    [vc viewDidAppear:YES];
+    _currentIndex = index;
+    _navTabBar.currentItemIndex = index;
+
 }
 
 - (void)shouldPopNavgationItemMenu:(BOOL)pop height:(CGFloat)height
@@ -186,6 +310,12 @@
         }];
     }
     [_navTabBar refresh];
+}
+
+// Gevin Added
+- (NSInteger)getCurrentIndex
+{
+    return _currentIndex;
 }
 
 @end
